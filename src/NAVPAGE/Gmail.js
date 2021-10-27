1d21fbba3ff784280f53c7f750d540645846f16d
NI Tushar <User@DESKTOP-EGNS6N2>
2021-10-28 02:20:08 +0600

    updated site

--- src/NAVPAGE/Gmail.js
+++ src/NAVPAGE/Gmail.js
@@ -2,29 +2,68 @@ import Footer from '../components/Footer';
 import '../Homepage.css'
 import '../navpageCSS/Gmail.css'
 import gmailposter from '../images/poster/gmailposter.jpg'
+import Gmailcard from '../components/gmail-price-card/GPriceCard'
 
-const Gmail=(props)=>{
+
+const Gmail=()=>{
     return (
         <>
-       {/* <div className="img-slider">
+       <div className="img-poster">
                <div className="blank-1">
                </div>
                <div className="poster">
                     <div>
                         <img src={gmailposter} className="gmail-poster" alt="" />
                     </div>
+                    <h2>Buy Gmail Accounts instant delivery at affordable prices</h2>
                     <div className="poster-desc">
+                        <h3>Gmail Accounts Features</h3>
                         <ul>
-                            <li>Description -1</li>
-                            <li>Description -1</li>
-                            <li>Description -1</li>
-                            <li>Description -1</li>
+                            <li>New And Fresh Account</li>
+                            <li>Phone Number Varified</li>
+                            <li>Recovery Email Added</li>
+                            <li>Instant Delivery</li>
                         </ul>
                     </div>
                </div>
                <div className="blank-2">
                </div>
-           </div> */}
+        </div>
+
+        <div className="about-gmail">
+               <div className="blank-1">
+               </div>
+               <div className="gmail-desc">
+                    <h4>What is the Gmail Accounts ?</h4>
+                    <p>Gmail is the email account with an email address. It’s launched in 2004 and provides 15GB of Storage for any Gmail account. Gmail is the only mail service in the world that email servers are very fast and secure. Every Gmail account has a specific username and that username called an email address.</p>
+
+                    <h4 className="gmail-benifit">The Benefit of the Gmail Accounts</h4>
+                    <p className="gmail-benifit-p">In the present world having a Gmail accounts is necessary the main reason is not only it helps you communicate with your friends, colleagues, and businesses but it also helps you surf any web registration in blogs to comment, subscribe to newsletters, and shop on the internet. and you will get many more facilities with Gmail Account.</p>
+               </div>
+               <div className="blank-2">
+               </div>
+        </div>
+
+        <div className="gmail-price-card-title">
+            <div className="blank-1">
+               </div>
+               <div className="g-price-card-title">
+                   <h3>Gmail Accounts Features And pricing</h3>
+               </div>
+               <div className="blank-2">
+            </div>
+        </div> 
+
+        <div className="gmail-price-card">
+               <div className="blank-1">
+               </div>
+               <div className="g-price-card">
+                <Gmailcard />
+               </div>
+               <div className="blank-2">
+               </div>
+        </div> 
+
 
        <div className="footer">
                <div className="blank-1">
@@ -34,7 +73,7 @@ const Gmail=(props)=>{
                </div>
                <div className="blank-2">
                </div>
-           </div> 
+        </div> 
         </>
     )
 }
---
+++ src/components/gmail-price-card/GPriceCard.js
@@ -0,0 +1,93 @@
+
+import './GPriceCard.css'
+const GPriceCard=()=>{
+    return (
+        <>
+       <div className="price-card">
+           <div className="g-card-body b-one">
+               <div className="bronze">
+                   <h3>Bronze</h3>
+               </div>
+                <div className="price1">
+                    <h1>$4</h1>
+                </div>
+                <div className="card-ul">
+                   
+                        <div className="one"><a>10 gmail Accounts</a></div>
+                        <div className="two"><a>Phone number verified</a></div>
+                        <div className="three"><a>Created by unique IP</a></div>
+                        <div className="four"><a>New and Fresh</a></div>
+                        <div className="five"><a>Recovery Email Added</a></div>
+                        <div className="six"><a>7 days replacement</a></div>
+                        <div className="seven"><a>100% Real Account</a></div>
+                        <div className="eight"><a>Instant Delivery</a></div>
+                    
+             <button className="g-card-btn1"><h5>Contact Now</h5></button>         
+                </div>
+           </div>
+
+           <div className="g-card-body b-two">
+               <div className="silver">
+                    <h3>Silver</h3>
+               </div>
+                <div className="price2">
+                    <h1>$8</h1>
+                </div>
+                <div className="card-ul">
+                    
+                        <div className="one"><a>10 gmail Accounts</a></div>
+                        <div className="two"><a>Phone number verified</a></div>
+                        <div className="three"><a>Created by unique IP</a></div>
+                        <div className="four"><a>New and Fresh</a></div>
+                        <div className="five"><a>Recovery Email Added</a></div>
+                        <div className="six"><a>7 days replacement</a></div>
+                        <div className="seven"><a>100% Real Account</a></div>
+                        <div className="eight"><a>Delivery 1-6 Hours</a></div>
+             <button className="g-card-btn2"><h5>Contact Now</h5></button>          
+                </div>
+           </div>
+
+           <div className="g-card-body b-three">
+               <div className="gold">
+                    <h3>Gold</h3>
+               </div>
+                <div className="price3">
+                    <h1>$18</h1>
+                </div>
+                <div className="card-ul">
+                        <div className="one"><a>10 gmail Accounts</a></div>
+                        <div className="two"><a>Phone number verified</a></div>
+                        <div className="three"><a>Created by unique IP</a></div>
+                        <div className="four"><a>New and Fresh</a></div>
+                        <div className="five"><a>Recovery Email Added</a></div>
+                        <div className="six"><a>7 days replacement</a></div>
+                        <div className="seven"><a>100% Real Account</a></div>
+                        <div className="eight"><a>Delivery 1-12 Hours</a></div>
+             <button className="g-card-btn3"><h5>Contact Now</h5></button>          
+                </div>
+           </div>
+
+            <div className="g-card-body b-four">
+               <div className="platinum">
+                    <h3>Platinum</h3>
+               </div>
+                <div className="price4">
+                   <h1>$36</h1>
+                </div>
+                <div className="card-ul">
+                        <div className="one"><a>10 gmail Accounts</a></div>
+                        <div className="two"><a>Phone number verified</a></div>
+                        <div className="three"><a>Created by unique IP</a></div>
+                        <div className="four"><a>New and Fresh</a></div>
+                        <div className="five"><a>Recovery Email Added</a></div>
+                        <div className="six"><a>7 days replacement</a></div>
+                        <div className="seven"><a>100% Real Account</a></div>
+                        <div className="eight"><a>Delivery 1-24 Hours</a></div>   
+             <button className="g-card-btn4"><h5>Contact Now</h5></button>           
+                </div>
+           </div>
+       </div>
+       </>
+    )
+}
+export default GPriceCard;
